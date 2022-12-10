1f94915da4eb92a8a6122bf296d8f3087798eec4
Anton Boev <110462969+t0H1k@users.noreply.github.com>
2022-12-10 10:42:46 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Anton Boev on 10.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
